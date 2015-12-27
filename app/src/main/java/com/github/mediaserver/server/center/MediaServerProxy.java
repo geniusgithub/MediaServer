0c83161499e6dbea479ea41360b7529ee09b4102
geniusgithub <lance.wyj@gmail.com>
2015-12-27 15:49:15 +0800

    update project to android studio

---
+++ app/src/main/java/com/github/mediaserver/server/center/MediaServerProxy.java
@@ -0,0 +1,52 @@
+package com.github.mediaserver.server.center;
+
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.github.mediaserver.ServerApplication;
+import com.github.mediaserver.server.DMSService;
+import com.github.mediaserver.util.CommonLog;
+import com.github.mediaserver.util.LogFactory;
+
+public class MediaServerProxy implements IBaseEngine{
+
+	private static final CommonLog log = LogFactory.createLog();
+	
+	private static  MediaServerProxy mInstance;
+	private Context mContext;
+	
+	private MediaServerProxy(Context context) {
+		mContext = context;
+	}
+
+	public static synchronized MediaServerProxy getInstance() {
+		if (mInstance == null){
+			mInstance  = new MediaServerProxy(ServerApplication.getInstance());
+		}
+		return mInstance;
+	}
+
+	@Override
+	public boolean startEngine() {
+		Intent intent = new Intent(DMSService.START_SERVER_ENGINE);
+		intent.setPackage(mContext.getPackageName());
+		mContext.startService(intent);
+		return true;
+	}
+
+	@Override
+	public boolean stopEngine() {
+		mContext.stopService(new Intent(mContext, DMSService.class));
+		return true;
+	}
+
+	@Override
+	public boolean restartEngine() {
+		Intent intent = new Intent(DMSService.RESTART_SERVER_ENGINE);
+		intent.setPackage(mContext.getPackageName());
+		mContext.startService(intent);
+		return true;
+	}
+
+}
